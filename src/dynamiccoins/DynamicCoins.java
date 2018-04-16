cee76ad05e0c7c9fad83364ddd58363873785754
Gavin Austin <2gaustin@gmail.com>
2018-04-16 15:13:08 -0600

    Added checks and tests for zero change and negative change. Also added names, date, and overview

--- src/dynamiccoins/DynamicCoins.java
+++ src/dynamiccoins/DynamicCoins.java
@@ -1,12 +1,29 @@
+/*
+ * Authors: Tyler Gentry, Gavin Austin
+ * Date: 24 April 2018
+ * Overview: This program extends the change-making problem to find the denomination 
+ * of each coin in the minimum number of coins that add up to the given amount of money.
+ * There is no main method. Execution is triggered by the JUnit test cases.
+ */
+
 package dynamiccoins;
 
 public class DynamicCoins {
 
-	public static void makeChange(int[] coins, int change) {
+	public static int makeChange(int[] coins, int change) {
+		int minCoins = change;
 		
-		if(coins.length == 0) { //if we receive an empty coin array
+		if (coins.length == 0) { //if we receive an empty coin array
 			throw new IllegalArgumentException("Array of size 0 not allowed");
 		}
+		//no solution if change is less than zero
+		if (change < 0) {
+			return 0;
+		}
+		//one solution if change is zero
+		if (change == 0) {
+			return 1;
+		}
+		return minCoins;
 	}
-	
 }
--- src/dynamiccoins/DynamicCoinsTest.java
+++ src/dynamiccoins/DynamicCoinsTest.java
@@ -1,3 +1,11 @@
+/*
+ * Authors: Tyler Gentry, Gavin Austin
+ * Date: 24 April 2018
+ * Overview: This program extends the change-making problem to find the denomination 
+ * of each coin in the minimum number of coins that add up to the given amount of money.
+ * There is no main method. Execution is triggered by the JUnit test cases.
+ */
+
 package dynamiccoins;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,9 +17,22 @@ class DynamicCoinsTest {
 
 	@Test
 	void emptyCoinArray() {
-		int[] array = {};
-		Exception e = assertThrows(IllegalArgumentException.class, () -> {  DynamicCoins.makeChange(array, 0); } );
+		int[] coins = {};
+		Exception e = assertThrows(IllegalArgumentException.class, () -> {  DynamicCoins.makeChange(coins, 0); } );
 		assertEquals("Array of size 0 not allowed", e.getMessage());	
 	}
-
+	
+	@Test
+	void negativeChange() {
+		int[] coins = {1, 5, 10, 25};
+		int change = -1;
+		assertEquals(0, DynamicCoins.makeChange(coins, change));
+	}
+	
+	@Test
+	void zeroChange() {
+		int[] coins = {1, 5, 10, 25};
+		int change = 0;
+		assertEquals(1, DynamicCoins.makeChange(coins, change));
+	}
 }
